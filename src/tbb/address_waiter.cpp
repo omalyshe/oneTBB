811cbfa16030982f39a616c1f4bcfd69cd8c5bd1
Olga Malysheva <olga.malysheva@intel.com>
2025-10-29 12:45:18 -0500

    Test CodeQL on changes in src

--- src/tbb/address_waiter.cpp
+++ src/tbb/address_waiter.cpp
@@ -13,6 +13,7 @@
     See the License for the specific language governing permissions and
     limitations under the License.
 */
+# Test change
 
 #include "oneapi/tbb/detail/_utils.h"
 #include "governor.h"
